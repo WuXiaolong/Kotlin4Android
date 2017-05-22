70d1abf22c94071526fe8a9a4388ab4e8a57a3fb
wuxiaolong.me <wuxiaolong.me@gmail.com>
2017-05-22 21:34:19 +0800

    main

--- app/src/main/java/com/wuxiaolong/kotlin4android/MainActivity.kt
+++ app/src/main/java/com/wuxiaolong/kotlin4android/MainActivity.kt
@@ -3,8 +3,9 @@ package com.wuxiaolong.kotlin4android
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+//冒号表示继承
 class MainActivity : AppCompatActivity() {
-
+    //问号表示该变量可以为空
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
